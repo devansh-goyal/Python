c26d5566329c29a13a2b27463eecdce2f8a39c57
devansh-goyal <devansh2002goyal@gmail.com>
2021-08-20 17:23:43 +0530

    Create escping escape character.py

---
+++ escping escape character.py
@@ -0,0 +1,7 @@
+print("C:\ users\timberchuka\notes.txt")
+print()
+# escaping with \
+print("C:\\users\\timberchuka\\notes.txt")
+print()
+# escaping with r before "
+print(r"C:\users\timberchuka\notes.txt")
