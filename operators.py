c59a9f634cc2c607ced86526a6fbfc1f008ffb00
devansh-goyal <devansh2002goyal@gmail.com>
2021-08-21 17:54:51 +0530

    CODES

---
+++ operators.py
@@ -0,0 +1,16 @@
+a = 12
+b = 4
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)         # division (with point)
+print(a//b)        # integer division(without point)
+print(a%b)         # remainder
+
+print()
+
+for i in range(1,4):   # we could write 'a//b' instead of 4 but we can't write 'a/b'(as it will give 4.0 not 4)
+    print(i)
+
+
---
+++ python is a strong lang..py
@@ -0,0 +1,11 @@
+name = "devansh"
+age = 18
+print(name + " is " + age + " years old")
+
+# 'int' value is converted automatically into 'str' by python because a person can find bugs easily by this.
+# On the other hand JAVA converts 'int' automatically to 'str' .
+
+# correct code for above
+# Name = "Devansh"
+# Age = "18"
+# print(Name + " is " + Age + " years old")
