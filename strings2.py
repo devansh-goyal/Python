914d10db955ac4cdfb422147104613b076515965
devansh-goyal <devansh2002goyal@gmail.com>
2021-08-23 16:35:18 +0530

    Code

---
+++ strings2.py
@@ -0,0 +1,30 @@
+# in python alloting numbers starts from 0
+# thats why norwegian blue have characters, numbered 0 to 14
+parrot = "Norwegian blue"
+print(parrot)
+# positive indexing
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+print("--------------------------------")
+# negative indexing
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+     #OR
+print()
+print("--------------------------------")
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
